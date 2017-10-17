8b30a5a3c49b598404059b8ee8dda9a35eba739e
gogotours <32586210+gogotours@users.noreply.github.com>
2017-10-17 16:05:14 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '172.30.59.125';
+$cfg['Servers'][$i]['host'] = '172.31.50.181';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
